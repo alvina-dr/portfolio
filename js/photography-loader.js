f82b5f030eb7cef640abfd8e5750ef065ba35196
Alvina DR <alvina@tavroxgames.com>
2022-12-07 22:24:46 +0100

    added tookana pictures

--- js/photography-loader.js
+++ js/photography-loader.js
@@ -5,13 +5,13 @@ var container3 = document.getElementById("photography-container3");
 LoadPhoto();
 
 function LoadPhoto() {
-    for (var i = 0; i < 26; i++) {
+    for (var i = 0; i < 32; i++) {
         var _photography = document.createElement("img");
         _photography.src = "../../img/photography/" + i + ".jpg";
         _photography.style.width = "100%";
-        if (i < 9) {
+        if (i < 11) {
             container1.appendChild(_photography);
-        }else if (i < 18){
+        }else if (i < 22){
             container2.appendChild(_photography);
         }else {
             container3.appendChild(_photography);
