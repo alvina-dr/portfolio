4aa0961c263d54357165914acd290d36a74c4367
Alvina DR <alvina@tavroxgames.com>
2022-12-07 23:38:07 +0100

    photography update

--- js/photography-loader.js
+++ js/photography-loader.js
@@ -5,13 +5,13 @@ var container3 = document.getElementById("photography-container3");
 LoadPhoto();
 
 function LoadPhoto() {
-    for (var i = 0; i < 32; i++) {
+    for (var i = 0; i < 30; i++) {
         var _photography = document.createElement("img");
         _photography.src = "../../img/photography/" + i + ".jpg";
         _photography.style.width = "100%";
         if (i < 11) {
             container1.appendChild(_photography);
-        }else if (i < 22){
+        }else if (i < 21){
             container2.appendChild(_photography);
         }else {
             container3.appendChild(_photography);
