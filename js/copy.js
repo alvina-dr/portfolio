5a79e4326e420c614340ef498fe8f8754dcfbbf9
alvina-dr <alvinadr24@gmail.com>
2025-08-31 20:19:31 +0200

    Add : copy mail adress

--- js/copy.js
+++ js/copy.js
@@ -1,18 +1,5 @@
-var textToCopy = document.getElementById('text-to-copy');
-var string = textToCopy.innerHTML;
-var tooltip = document.getElementById('tooltip');
-
-function CopyText() {
-    navigator.clipboard.writeText(string);
-    // alert("Copied");
-    tooltip.animate([
-        {color: 'transparent'},
-        {color: 'var(--primary-color)',
-    backgroundColor: 'var(--dark-color)' },
-    {color: 'var(--primary-color)',
-    backgroundColor: 'var(--dark-color)' },
-        {color: 'transparent'}
-      ], {
-        duration: 1000,
-      });
-    }
+function CopyText() 
+{
+    navigator.clipboard.writeText("alvinadr24@gmail.com");
+    alert("Copied");
+}
